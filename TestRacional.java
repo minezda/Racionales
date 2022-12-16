1e81129c4e230903da7fe1630375bf43b5e07450
minezda <86487722+minezda@users.noreply.github.com>
2022-12-15 22:14:40 -0600

    Create TestRacional.java

---
+++ TestRacional.java
@@ -0,0 +1,13 @@
+
+public class TestRacional
+{
+    public static void main(String[]args)
+    {
+        CRacional r1 = new CRacional();
+        CRacional r2 = new CRacional();
+        CRacional r3 = new CRacional();
+        r1.asignacion(3,4);
+        r2.asignacion(6,3);
+        r3.suma(r1,r2);
+    }
+}
